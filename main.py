4a753fbb613882cfe48b38c8712f60871d5a7a63
Advance Team <advance@none.com>
2022-04-16 18:36:34 +0300

    v.0.1.5

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import cv2 as cv
 from deepface import DeepFace
 from datetime import datetime as dt
 from flask import Flask, jsonify, render_template, request, send_from_directory
-backends = ['opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface', 'mediapipe']
+backends = ['mediapipe', 'opencv', 'ssd', 'dlib', 'mtcnn', 'retinaface']
 models = ["VGG-Face", "Facenet", "Facenet512", "OpenFace", "DeepFace", "DeepID", "ArcFace", "Dlib"]
 
 app = Flask(__name__)
@@ -21,8 +21,8 @@ def send_report(path):
 @app.route('/')
 @app.route('/index.html')
 def index():
-    return render_template('index.html')
-    
+    return render_template('index.html', backends=backends)
+
 def archive():
     return render_template('archive.html')
 
@@ -31,7 +31,10 @@ def result():
     global free
     if request.method == 'POST':
         req = request.get_data().decode("utf-8")
-        result = json.loads(req)['image'][22:]
+        jsonData = json.loads(req)
+        actions = jsonData['actions']
+        backend = jsonData['backend']
+        result = jsonData['image'][22:]
         result = base64.b64decode(result)
         req = cv.imdecode(np.fromstring(result, np.uint8), cv.IMREAD_COLOR)
         small_frame = cv.resize(req, (0, 0), None, 0.25, 0.25)
@@ -40,7 +43,7 @@ def result():
         free = False
         try:
             result = DeepFace.analyze(
-                small_frame, actions=["age", "gender", "emotion", "race"], detector_backend=backends[5])
+                small_frame, actions=actions, detector_backend=backend)
         except ValueError:
             free = True
             return jsonify({"err": "No face", "code": 0})
